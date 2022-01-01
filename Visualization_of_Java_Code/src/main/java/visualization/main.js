5f8557b04c8cc0dd798ab6e50e66eac99b5487f3
Veronika Semmelrock <semmelrockveronika@gmail.com>
2022-01-01 20:40:19 +0100

    First display of all elements in their hierarchy, layout does not work yet, vertex design done

--- Visualization_of_Java_Code/src/main/java/visualization/main.js
+++ Visualization_of_Java_Code/src/main/java/visualization/main.js
@@ -28,27 +28,49 @@ async function main(container){
         new mxRubberband(graph); //put rubberband around graph so graph can be moved around
         let parent = graph.getDefaultParent(); 
         graph.getModel().beginUpdate();
-        let x = 1; 
-        let y= 1;
+        let x = 0; 
+        let y= 0;
+        let xUpperLayer = 0; 
+        let yUpperLayer =0; 
         const vertexes = [];
+        let newtype; 
+        let oldtype; 
+        let width; 
+        let height; 
     
+        let pkgOffsetX = 300;
+        let pkgOffsetY = 0;
+        let pkgColumns
 
         try{
             Object.keys(entities).forEach(function(key){
 
-                //deciding on shape/style
+                //deciding on shape/style and correct coordinates
                 switch (entities[key].fType){
                     case "class": 
-                       style = "SHAPE_RECTANGLE;ROUNDED;fillColor=#D0DDFF";  //style from mxConstants.js
+                       style = "SHAPE_RECTANGLE;rounded=1;fillColor=#D0DDFF";  // blue; style from mxConstants.js
+                       newtype = "class";
+                       width = 200; 
+                       height = 50; 
                        break; 
                     case "package":
-                        style = "SHAPE_RECTANGLE;fillColor=#FAFFB0"; 
+                        style = "SHAPE_RECTANGLE;rounded=1;fillColor=#FAFFB0"; // yellow
+                        newtype = "package";
+                        width = 300; 
+                        height = 300; 
+                        pkgColumns ++
                         break; 
                     case "method": 
-                        style = "SHAPE_RHOMBUS;fillColor=#FFC6D6"; 
+                        style = "SHAPE_RHOMBUS;rounded=1;fillColor=#FFC6D6"; // red
+                        newtype = "method";
+                        width = 180; 
+                        height = 30;
                         break; 
                     case "attribute": 
-                        style = "SHAPE_ELLIPSE;fillColor=#FAFFB0"; 
+                        style = "SHAPE_ELLIPSE;rounded=1;fillColor=#C0FFB6"; // green
+                        newtype = "attribute";
+                        width = 180; 
+                        height = 30;
                         break; 
                     default: 
                         alert("Object "+key+" did not have a correclty set type for choosing shape");
@@ -65,13 +87,39 @@ async function main(container){
                         }
                     })
                 }
-                console.log(parent, entities[key].fUniqueName, entities[key].fUniqueName, x, y, 70, 30, style)
-                //creating vertexes
-                vertexes.push(graph.insertVertex(parent, entities[key].fUniqueName, entities[key].fUniqueName, x, y, 70, 30, style));
-                x+=50; 
-                y+=50; 
 
-                
+                if(pkgColumns === 3) {
+                    pkgColumns = 0;
+                    pkgOffsetX = 0;
+                    pkgOffsetY += 400;
+                }
+                //setting x and y coordinates relative to parent correctly - because hashmap is structured every time a new
+                //type is encountered x and y must be set back to 0 because a new recursive layer is entered
+                //this is not the case with the uppermost layer, these need to be structured seperately 
+                if(!oldtype){
+                    oldtype = newtype; 
+                }else if(newtype !== oldtype){
+                    //uppermost layer must be structured well too
+                    if(parent == graph.getDefaultParent()){
+                        x = xUpperLayer; 
+                        y = yUpperLayer; 
+                        xUpperLayer += pkgOffsetX; 
+                        yUpperLayer += pkgOffsetY; 
+                    }else{
+                        x=0; 
+                        y=0;  
+                    }
+                     
+                    oldtype = newtype; 
+                }else{
+                    x+=20; 
+                    y+=20; 
+                }
+
+                console.log(parent, entities[key].fUniqueName, entities[key].fUniqueName, x, y, width, height, style)
+                //creating vertexes
+                vertexes.push(graph.insertVertex(parent, entities[key].fUniqueName, entities[key].fUniqueName, x, y, width, height, style));
+                     
             });
         } finally{
             graph.getModel().endUpdate();
