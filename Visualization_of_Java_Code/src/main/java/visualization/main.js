898491568715f4e2c5476337bebc2e7f8f8fc48a
Veronika Semmelrock <semmelrockveronika@gmail.com>
2022-01-03 11:44:13 +0100

    Refactoring / Code Cleanup

--- Visualization_of_Java_Code/src/main/java/visualization/main.js
+++ Visualization_of_Java_Code/src/main/java/visualization/main.js
@@ -1,10 +1,22 @@
+const MYCOLOUR_BLUE = "#D0DDFF";
+const MYCOLOUR_GREEN = "#C0FFB6";
+const MYCOLOUR_RED = "#FFC6D6";
+const MYCOLOUR_YELLOW = "#FAFFB0"; 
+const STYLE_CLASS = "rounded=1;shape=rectangle;fillColor="+MYCOLOUR_BLUE;
+const STYLE_PACKAGE = "shape=rectangle;rounded=1;fillColor="+MYCOLOUR_YELLOW;
+const STYLE_METHOD = "shape=rhombus;fillColor="+MYCOLOUR_RED;
+const STYLE_ATTRIBUTE = "shape=ellipse;fillColor="+MYCOLOUR_GREEN;
+
 let assocs;
 let entities;
+const vertices=[]; 
+const parents=[];
+let graph; 
 
 const body = document.getElementById('root');
 const graphContainer = document.getElementById('graphContainer');
 body.onload = main(graphContainer); //calls main when body is finished loading
-
+ 
 //loads files, interprets json string in file and saves parsed hashmap into variables
 //async means asynchronous -> other code is / can be executed while these methods work
 //fetch() and json() are asynchronous methods, to call those methods await must be used
@@ -18,94 +30,24 @@ async function loadFiles(){
     }
 }
 
+
 //because loadFiles() (async method) is called, main() must be async (to be able to call await)
 async function main(container){
+    let parent; 
+    let width; 
+    let height; 
+    let style; 
     await loadFiles();
     if(!mxClient.isBrowserSupported()){
         alert("Browser not supported!");
     }else{
-        let graph = new mxGraph(container);//create variable holding mxGraph and add to container
-        new mxRubberband(graph); //put rubberband around graph so graph can be moved around
-        let parent = graph.getDefaultParent(); 
-        graph.getModel().beginUpdate();
-        let x = 0; 
-        let y= 0; 
-        const vertices = [];
-        const parents =[]; //remembering parents for layouting
-        let width; 
-        let height;  
-
+        graph = createGraph(container);  
+        graph.getModel().beginUpdate(); 
         try{
-            var layout = new mxHierarchicalLayout(graph);
-            
-            layout.resizeParent = true;//resize parent so parent is able to hold all children 
-            layout.moveParent = false; 
-            
-            layout.parentBorder = 10;//border size between children of parent and parent border 
-            layout.intraCellSpacing = 20; 
-            layout.interRankCellSpacing = 0; 
-            layout.interHierarchySpacing = 20; //spacing between seperate hierarchies
-
-            //layout.parallelEdgeSpacing = 0; //Edge bundling? -- or adding another layouting algorithm
-            //layout.orientation = mxConstants.DIRECTION_WEST; 
-            layout.fineTuning = true; 
-            layout.tightenToSource = true; 
-            //layout.disableEdgeStyle = false; //makes custom edge style possible
-
-            layout.traverseAncestors = true;
-
-            Object.keys(entities).forEach(function(key){
-                //deciding on shape/style
-                switch (entities[key].fType){
-                    case "class": 
-                       style = "rounded=1;fillColor=#D0DDFF;shape=rectangle";  // blue; style from mxConstants.js                       
-                       break; 
-                    case "package":
-                        style = "shape=rectangle;rounded=1;fillColor=#FAFFB0"; // yellow
-                        break; 
-                    case "method": 
-                        style = "fillColor=#FFC6D6;shape=rhombus"; // red
-                        break; 
-                    case "attribute": 
-                        style = "fillColor=#C0FFB6;shape=ellipse"; // green
-                        break; 
-                    default: 
-                        alert("Object "+key+" did not have a correclty set type for choosing shape");
-                        style = null; 
-                        break;
-                }
-                
-                //getting correct parent for hierarchical structure
-                if(entities[key].fParentAsString == "null"){
-                    parent = graph.getDefaultParent();
-                    height = 100; 
-                    width = 100; 
-                }else{
-                    parent = vertices.find(function(vertex){
-                        if(vertex.id == entities[key].fParentAsString){
-                            parents.push(vertex); 
-                            return vertex
-                        }
-                    })
-                    height = 20; 
-                    width = 20; 
-                }
-                //console.log(parent, entities[key].fUniqueName, entities[key].fUniqueName, x, y, width, height, style)
-                //creating vertices
-                vertices.push(graph.insertVertex(parent, entities[key].fUniqueName, entities[key].fUniqueName, x, y, height, width, style));
-                     
-            });
-
-            //layout.execute(graph.getDefaultParent(), parents);
-            layout.execute(graph.getDefaultParent(), graph.getDefaultParent().getChildren())
-            /*layout.execute(graph.getDefaultParent()); 
-            parents.forEach(function(parent){
-                layout.execute(parent);
-            })
-            
-            //??? different layout if both execute statements are executed?? - hm 
-            //make use of findRoots()??
-
+            let layout = createHierarchicalLayout(graph); 
+            insertVertices(); 
+            executeLayout(layout); 
+          
             //inserting dummy vertices
             /*vertices.forEach(function(vertex, i){
                 graph.insertEdge(graph.getDefaultParent, null, "tests", vertex, vertices[i+1]); 
@@ -117,3 +59,100 @@ async function main(container){
         }
     }
 };
+
+
+function createGraph(container){
+    let graph = new mxGraph(container); 
+    new mxRubberband(graph); 
+    return graph;
+}
+
+function createHierarchicalLayout(graph){
+    var layout = new mxHierarchicalLayout(graph);   
+    layout.resizeParent = true;//resize parent so parent is able to hold all children 
+    layout.moveParent = false; //move parent if resizing is called
+    layout.parentBorder = 10;//border size between children of parent and parent border 
+    layout.intraCellSpacing = 20; 
+    layout.interRankCellSpacing = 0; 
+    layout.interHierarchySpacing = 20; //spacing between seperate hierarchies
+    //layout.parallelEdgeSpacing = 0; //Edge bundling? -- or adding another layouting algorithm
+    //layout.orientation = mxConstants.DIRECTION_WEST; 
+    layout.fineTuning = true; 
+    layout.tightenToSource = true; 
+    //layout.disableEdgeStyle = false; //makes custom edge style possible
+    //layout.traverseAncestors = true;
+    return layout; 
+}
+
+function getStyle(fType, key){
+    let style; 
+    //deciding on shape/style
+    switch (fType){
+        case "class": 
+           style = STYLE_CLASS;                       
+           break; 
+        case "package":
+            style = STYLE_PACKAGE; 
+            break; 
+        case "method": 
+            style = STYLE_METHOD; 
+            break; 
+        case "attribute": 
+            style = STYLE_ATTRIBUTE; 
+            break; 
+        default: 
+            alert("Object "+key+" did not have a correclty set type for choosing shape");
+            style = null; 
+            break;
+    }
+    return style; 
+}
+
+function getParent(parentString){
+    //getting correct parent for hierarchical structure
+    if(parentString == "null"){
+        parent = graph.getDefaultParent();
+    }else{
+        parent = vertices.find(function(vertex){
+            if(vertex.id == parentString){
+                parents.push(vertex); 
+                return vertex;
+            }
+        })
+    }
+    return parent; 
+}
+
+function getHeight(parent){
+    if(parent === graph.getDefaultParent()){
+        return 100; 
+    }else{
+        return 20;
+    } 
+}
+
+function getWidth(parent){
+    if(parent === graph.getDefaultParent()){
+        return 100; 
+    }else{
+        return 20;
+    }
+}
+
+function insertVertices(){
+    Object.keys(entities).forEach(function(key){//looping through each entity
+        style = getStyle(entities[key].fType, key); //get style depending on what type of entity it is 
+        parent = getParent(entities[key].fParentAsString); //get parent for correct hierarchical structure
+        width = getWidth(parent); 
+        height = getHeight(parent); 
+        vertices.push(graph.insertVertex(parent, entities[key].fUniqueName, entities[key].fUniqueName, 0, 0, height, width, style));   
+    });
+}
+
+function executeLayout(layout){
+    //executing layout - ?? slighty different statements perform totally different layout
+    layout.execute(graph.getDefaultParent(), graph.getChildVertices(graph.getDefaultParent()));
+    parents.forEach(function(parent){
+        layout.execute(parent, graph.getChildVertices(parent));
+    })
+}
