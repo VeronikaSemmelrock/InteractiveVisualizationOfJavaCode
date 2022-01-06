d75ff85b8a63bd66a2f72c24a3d6e8d14721ea1b
Veronika Semmelrock <semmelrockveronika@gmail.com>
2022-01-06 16:25:04 +0100

    Encountered some version problems, but now -> Screen Panning (Scrolling) works, Layouting almost perfect, show/hide button for folding and folding itself works (src of mxGraph was added)!

--- Visualization_of_Java_Code/src/main/java/visualization/main.js
+++ Visualization_of_Java_Code/src/main/java/visualization/main.js
@@ -1,26 +1,22 @@
 const MYCOLOUR_BLUE = "#D0DDFF";
 const MYCOLOUR_GREEN = "#C0FFB6";
 const MYCOLOUR_RED = "#FFC6D6";
-const MYCOLOUR_YELLOW = "#FAFFB0"; 
-const STYLE_CLASS = "rounded=1;shape=rectangle;fillColor="+MYCOLOUR_BLUE;
-const STYLE_PACKAGE = "shape=rectangle;rounded=1;fillColor="+MYCOLOUR_YELLOW;
-const STYLE_METHOD = "shape=rhombus;fillColor="+MYCOLOUR_RED;
-const STYLE_ATTRIBUTE = "shape=ellipse;fillColor="+MYCOLOUR_GREEN;
+const MYCOLOUR_YELLOW = "#FAFFB0";
+const STYLE_CLASS = "autosize=1;shape=rectangle;fillColor="+MYCOLOUR_BLUE;
+const STYLE_PACKAGE = "autosize=1;shape=rectangle;fillColor="+MYCOLOUR_YELLOW;
+const STYLE_METHOD = "autosize=1;shape=rectangle;fillColor="+MYCOLOUR_RED;
+const STYLE_ATTRIBUTE = "autosize=1;shape=ellipse;fillColor="+MYCOLOUR_GREEN;
 
 let assocs;
 let entities;
-const vertices=[]; 
+const vertices=[];
 const parents=[];
-let graph; 
+let graph;
 
 const body = document.getElementById('root');
 const graphContainer = document.getElementById('graphContainer');
 body.onload = main(graphContainer); //calls main when body is finished loading
- 
-//loads files, interprets json string in file and saves parsed hashmap into variables
-//async means asynchronous -> other code is / can be executed while these methods work
-//fetch() and json() are asynchronous methods, to call those methods await must be used
-//to be able to use await the method loadFiles() itself must be asynchronous
+
 async function loadFiles(){
     try{
         assocs = await (await fetch('/assocs.json')).json();
@@ -30,31 +26,17 @@ async function loadFiles(){
     }
 }
 
-
 //because loadFiles() (async method) is called, main() must be async (to be able to call await)
 async function main(container){
-    let parent; 
-    let width; 
-    let height; 
-    let style; 
+    let parent;
+    let width;
+    let height;
+    let style;
     await loadFiles();
     if(!mxClient.isBrowserSupported()){
         alert("Browser not supported!");
     }else{
-        graph = createGraph(container); 
-        
-        
-        /////////code for folding (show/hide of groups)
-        graph.setDropEnabled(true);
-				
-        // Disables global features
-        graph.collapseToPreferredSize = false;
-        graph.constrainChildren = false;
-        graph.cellsSelectable = false; //nothing can be moved around anymore
-        graph.extendParentsOnAdd = false;
-        graph.extendParents = false;
-        graph.border = 100;//changes a lot too - TODO - what is that? 
-
+        graph = createGraph(container);
         // Installs auto layout for all levels
         var layout = new mxStackLayout(graph, true);
         layout.border = graph.border;
@@ -73,28 +55,22 @@ async function main(container){
                 {
                     layout.resizeParent = true;
                     layout.horizontal = true;
-                    layout.spacing = 40;
+                    layout.spacing = 50;
                 }
-                
                 return layout;
             }
-            
             return null;
         };
-        // Resizes the container
-		graph.setResizeContainer(true);
-        /////////
 
-
-        graph.getModel().beginUpdate(); 
+        graph.getModel().beginUpdate();
         try{
-            let layout = createHierarchicalLayout(graph); 
-            insertVertices();         
-            executeLayout(layout); 
-          
+            let layout = createHierarchicalLayout(graph);
+            insertVertices();
+            executeLayout(layout);
+
             //inserting dummy vertices
             /*vertices.forEach(function(vertex, i){
-                graph.insertEdge(graph.getDefaultParent, null, "tests", vertex, vertices[i+1]); 
+                graph.insertEdge(graph.getDefaultParent, null, "tests", vertex, vertices[i+1]);
             })
             */
 
@@ -106,53 +82,67 @@ async function main(container){
 
 
 function createGraph(container){
-    let graph = new mxGraph(container); 
-    new mxRubberband(graph); 
+    let graph = new mxGraph(container);
+    /////////code for folding (show/hide of groups)
+    graph.setDropEnabled(true);
+    // Enables automatic sizing for vertices after editing and
+    // panning by using the left mouse button.
+    graph.setAutoSizeCells(true);
+    graph.setPanning(true);
+    //graph.panningHandler.useLeftButtonForPanning = true;
+    graph.collapseToPreferredSize = false;
+    graph.constrainChildren = false;
+    graph.cellsSelectable = false; //nothing can be moved around anymore!
+    graph.extendParentsOnAdd = false;
+    graph.extendParents = false;
+    graph.border = 20;
+    graph.setResizeContainer(true);
+    new mxRubberband(graph);
     //graph.setResizeContainer(true);?
     return graph;
 }
 
 function createHierarchicalLayout(graph){
-    var layout = new mxHierarchicalLayout(graph);   
-    layout.resizeParent = true;//resize parent so parent is able to hold all children 
+    var layout = new mxHierarchicalLayout(graph);
+    // layout.resizeParent = true;//resize parent so parent is able to hold all children
     layout.moveParent = false; //move parent if resizing is called
-    layout.parentBorder = 10;//border size between children of parent and parent border 
-    layout.intraCellSpacing = 20; 
-    layout.interRankCellSpacing = 0; 
-    layout.interHierarchySpacing = 20; //spacing between seperate hierarchies
+    layout.parentBorder = 10;//border size between children of parent and parent border
+    layout.intraCellSpacing = 20;
+    layout.interRankCellSpacing = 0;
+    layout.interHierarchySpacing = 13; //spacing between seperate hierarchies
     //layout.parallelEdgeSpacing = 0; //Edge bundling? -- or adding another layouting algorithm
-    //layout.orientation = mxConstants.DIRECTION_WEST; 
-    layout.fineTuning = true; 
-    layout.tightenToSource = true; 
+    //layout.orientation = mxConstants.DIRECTION_WEST;
+    layout.fineTuning = true;
+    layout.tightenToSource = true;
     layout.border = graph.border;
     //layout.disableEdgeStyle = false; //makes custom edge style possible
     //layout.traverseAncestors = true;
-    //layout.useBoundingBox=false; 
-    return layout; 
+    //layout.useBoundingBox=false;
+    return layout;
 }
 
 function getStyle(fType, key){
-    let style; 
+    let style;
     //deciding on shape/style
     switch (fType){
-        case "class": 
-           style = STYLE_CLASS;                       
-           break; 
+        case "class":
+           style = STYLE_CLASS;
+           break;
         case "package":
-            style = STYLE_PACKAGE; 
-            break; 
-        case "method": 
-            style = STYLE_METHOD; 
-            break; 
-        case "attribute": 
-            style = STYLE_ATTRIBUTE; 
-            break; 
-        default: 
+            style = STYLE_PACKAGE;
+            break;
+        case "method":
+            style = STYLE_METHOD;
+            break;
+        case "attribute":
+            style = STYLE_ATTRIBUTE;
+            break;
+        default:
             alert("Object "+key+" did not have a correclty set type for choosing shape");
-            style = null; 
+            style = null;
             break;
     }
-    return style; 
+    return style;
 }
 
 function getParent(parentString){
@@ -162,48 +152,45 @@ function getParent(parentString){
     }else{
         parent = vertices.find(function(vertex){
             if(vertex.id == parentString){
-                parents.push(vertex); 
+                parents.push(vertex);
                 return vertex;
             }
         })
     }
-    return parent; 
+    return parent;
 }
 
-function getHeight(parent){
+function getHeight(parent){//height of children of defaultParent are autoresized.
+    //Rest must be sized depending on which hierarchy layer they are in - TODO
+    let height = innerHeight - 500 //value doesnt matter bc of autoresize
+    if(parent.parent.geometry) height = parent.parent.geometry.height // != null&&!=undefined
     if(parent === graph.getDefaultParent()){
-        return 100; 
+        return height;
     }else{
-        return 20;
-    } 
-}
-
-function getWidth(parent){
-    if(parent === graph.getDefaultParent()){
-        return 100; 
-    }else{
-        return 20;
+        return 80; // only this matters but can be adjusted based on parent geometry
     }
+}
+
+function getWidth(parent){//TODO
+    let width = 250 // hard coded <-- get the root containers (children of defaultParent) and divide the innerWidth by the number of containers <-- width gets smaller if there are more containers
+    if(parent.parent.geometry) width = parent.parent.geometry.width
+    if(parent === graph.getDefaultParent()) return width;
+    else if((parent.style).includes(MYCOLOUR_BLUE)) return width - 75;
+    else return width - 40;
 }
 
 function insertVertices(){
     Object.keys(entities).forEach(function(key){//looping through each entity
-        style = getStyle(entities[key].fType, key); //get style depending on what type of entity it is 
+        style = getStyle(entities[key].fType, key); //get style depending on what type of entity it is
         parent = getParent(entities[key].fParentAsString); //get parent for correct hierarchical structure
-        width = getWidth(parent); 
-        height = getHeight(parent); 
-        vertices.push(graph.insertVertex(parent, entities[key].fUniqueName, entities[key].fUniqueName, 0, 0, height, width, style));   
+        width = getWidth(parent);
+        height = getHeight(parent);
+        vertices.push(graph.insertVertex(parent, entities[key].fUniqueName, entities[key].fUniqueName, 0, 0, width, height, style));
     });
 }
 
 function executeLayout(layout){
-    //executing layout - ?? slighty different statements perform totally different layout
-    //do i need to call execute from inside out? so from end of array to start of array? 
-    
     parents.forEach(function(parent){
         layout.execute(parent, graph.getChildVertices(parent));
     })
-    
-    //layout.execute(graph.getDefaultParent(), graph.getChildVertices(graph.getDefaultParent()));
-    //layout.execute(graph.getDefaultParent()); 
 }
