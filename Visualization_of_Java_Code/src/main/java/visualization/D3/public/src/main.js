945f116137b7cec2ccaf2015fc1b155446b5189c
veronika <veronika@isys.uni-klu.ac.at>
2022-11-23 18:46:55 +0100

    Working on showing/hiding nodes via click, code cleanup

--- Visualization_of_Java_Code/src/main/java/visualization/D3/public/src/main.js
+++ Visualization_of_Java_Code/src/main/java/visualization/D3/public/src/main.js
@@ -1,33 +1,60 @@
-// import * as d3 from "/d3/index.js"
-// import * as cola from "/webcola/dist/index.js";
-import graph from "./smallgrouped.js"
-
-// import "./style-base.css";
-// import "./style-diagram.css";
+import data from "./smallgrouped.js"
 const d3 = window.d3
-// const cola = window.webcol
 
-console.log('AAAHHHHHHHHHHHH', d3, d3Cola)
+//for window
 var width = 700,
     height = 700;
 var margin = 20,
     pad = 8;
 
+//setting colour scheme
 var color = d3.scaleOrdinal(d3.schemeSet3);
+
+//configuring webcola
 var d3Cola = cola
     .d3adaptor(d3)
     .linkDistance(80)
     .avoidOverlaps(true)
     .handleDisconnected(false)
     .symmetricDiffLinkLengths(51)
+    //.flowLayout("x", 150)  //the call to flowLayout causes all edges not involved in a cycle to have a separation constraint generated between their source and sink
+    // with a minimum spacing set to 150. Specifying the 'x' axis achieves a left-to-right flow layout. The default is top-to-bottom flow layout
     .size([width, height]);
 
+/*
+//for zoom and panning later 
+var container = d3
+    .select("#diagram")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("pointer-events", "all");
+
+container.append('rect')
+    .attr('class', 'background')
+    .attr('width', "100%")
+    .attr('height', "100%")
+    .call(d3.behavior.zoom().on("zoom", redraw));    
+
+var svg = container.append("g"); 
+
+//for zooming panning
+var nodeMouseDown = false;
+
+function redraw() {
+    if (nodeMouseDown) return;
+    svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")");
+}
+*/
+
+//appending svg to container "diagram"
 var svg = d3
     .select("#diagram")
     .append("svg")
     .attr("width", width)
     .attr("height", height);
 
+//configuring appended svg  
 svg
     .append("svg:defs")
     .append("svg:marker")
@@ -42,188 +69,264 @@ svg
     .attr("stroke-width", "0px")
     .attr("fill", "#000");
 
-graph.nodes.forEach(function (v) {
+//setting width and height of each node in 
+data.nodes.forEach(function (v) {
     v.width = v.height = 95;
 });
-graph.groups.forEach(function (g) {
+data.groups.forEach(function (g) {
     g.padding = 5; //changed from 0.05
 });
 
-d3Cola //setting data into d3Cola
-    .nodes(graph.nodes)
-    .links(graph.links)
-    .groups(graph.groups)
-    .start(100, 0, 50, 50);
+//update lists so nodes and links only holds data that should be drawn (e.g. visibility true) 
+var nodes = updateData(data.nodes); 
+var links = updateData(data.links);
+//var groups = updateData(graph.groups, nodes);
+//console.log("Groups --> ", groups)
+//start drawing process with data 
+redraw(); 
 
-var group = svg
-    .selectAll(".group")
-    .data(graph.groups)
-    .enter()
-    .append("rect")
-    .attr("rx", 8) //how much rounding
-    .attr("ry", 8) //how much rounding
-    .attr("class", "group")
-    .style("fill", function (d, i) {
-        return color(i);
-        return "#000000";
-    })
-    .call(d3Cola.drag)
-    .on("mouseup", function (d) {
-        d.fixed = 0;
-        d3Cola.alpha(1); // fire it off again to satify gridify
+    
+/* Poosible ways to filter
+.attr("visibility", "hidden");//possibility for hiding/showing, but layout stays the same (not compressed) and labels are still visible 
+.join(//define enter, update and exit functions/behaviour
+    /*function(enter) {
+        return enter.append('circle')
+        .style('opacity', 0.25);
+    },
+
+    function(update) {
+        return update.style('opacity', 1);
+    }, 
+    function(exit) {
+        return exit.remove();
+        }
+)*/
+
+//returns a list of all data that should be drawn (nodes or links that have visibility set to true)
+function updateData(datalist){
+    return datalist.filter(data => data.visibility)
+}
+
+//sets node visibility of node in data-list to false 
+function setNodeVisibility(nodeId){
+    data.nodes.forEach(node => {
+        if(node.id === nodeId){
+            node.visibility = false; 
+        }    
     });
+    //let node = graph.nodes.filter(node => node.id ===nodeId)
+}
 
-var link = svg
-    .selectAll(".link")
-    .data(graph.links)
-    .enter()
-    .append("path") //arrows, line would make lines
-    .attr("class", "link");
+//handles click by setting visibility of node to false and redrawing 
+function handleNodeClick(nodeId){
+    setNodeVisibility(nodeId)//sets node visibility of node in data-list to false 
+    nodes = updateData(graph.nodes);//updates node list that is used for drawing because data list of nodes changed 
+    redraw(); 
+}
 
-var pad = 20;
-var node = svg
-    .selectAll(".node")
-    .data(graph.nodes)
-    .enter()
-    .append("rect")
-    .attr("class", "node")
-    .attr("width", function (d) {
-        return d.width - 2 * pad;
-    })
-    .attr("height", function (d) {
-        return d.height - 2 * pad;
-    })
-    .attr("rx", 5) //rounding
-    .attr("ry", 5)
-    .style("fill", function (d) {
-        // return "#000000";
-        return color(graph.groups.length);
-    })
-    .call(d3Cola.drag)
-    .on("mouseup", function (d) {
-        d.fixed = 0;
-        d3Cola.alpha(1); // fire it off again to satify gridify
+/*
+//first incomplete version for setting and updating groups correctly 
+function updateGroups(datalist, nodelist){
+    let result = []; 
+    datalist.forEach(group => {
+        if(group.visibility === true){
+            //check if all leaves in group exist 
+            group.leaves.forEach(leaf )
+        }    
     });
+}*/
 
-var label = svg
-    .selectAll(".label")
-    .data(graph.nodes)
-    .enter()
-    .append("text")
-    .attr("class", "label")
-    .text(function (d) {
-        return d.name;
-    })
-    .call(d3Cola.drag);
+//(re)drawing of graph 
+function redraw(){ 
+    console.log("Nodes in redraw --> ", nodes)   
+    d3Cola //setting data into d3Cola
+        .nodes(nodes)
+        .links(links)
+        .groups(data.groups)
+        .start(50, 0, 50);
 
-node.append("title").text(function (d) {
-    return d.name;
-});
-
-//my code
-var groupLabel = svg
-    .selectAll(".grouplabel")
-    .data(graph.groups)
-    .enter()
-    .append("text")
-    .attr("class", "grouplabel")
-    .text(function (d) {
-        return d.name;
-    })
-    .call(d3Cola.drag);
-
-group.append("title").text(function (d) {
-    return d.name;
-});
-
-console.log("group", group);
-console.log("node", node);
-
-var lineFunction = d3
-    .line()
-    .x(function (d) {
-        return d.x;
-    })
-    .y(function (d) {
-        return d.y;
-    })
-    .curve(d3.curveLinear);
-
-d3Cola.on("tick", function () {
-    node
-        .each(function (d) {
-            d.innerBounds = d.bounds.inflate(-margin);
+    //inserting groups into svg 
+    var group = svg
+        .selectAll(".group")
+        .data(data.groups)
+        .enter()
+        .append("rect")
+        .attr("rx", 8) //how much rounding
+        .attr("ry", 8) //how much rounding
+        .attr("class", "group")
+        .style("fill", function (d, i) {
+            return color(i);
         })
-        .attr("x", function (d) {
-            return d.innerBounds.x;
-        })
-        .attr("y", function (d) {
-            return d.innerBounds.y;
-        })
+        .call(d3Cola.drag)
+        .on("mouseup", function (d) {
+            d.fixed = 0;
+            d3Cola.alpha(1); // fire it off again to satify gridify
+        }); 
+    
+    //inserting links into svg
+    var link = svg
+        .selectAll(".link")
+        .data(links)
+        .enter()
+        .append("path") //arrows, line would make lines
+        .attr("class", "link"); 
+
+    var pad = 20;
+    //inserting nodes into svg 
+    var node = svg
+        .selectAll(".node")
+        .data(nodes)
+        .enter()
+        .append("rect")
+        .attr("class", "node")
         .attr("width", function (d) {
-            return d.innerBounds.width();
+            return d.width - 2 * pad;
         })
         .attr("height", function (d) {
-            return d.innerBounds.height();
+            return d.height - 2 * pad;
+        })
+        .attr("rx", 5) //rounding
+        .attr("ry", 5)
+        .style("fill", function (d) {
+            return "#00000" 
+            //color(graph.groups.length);
+        })
+        .call(d3Cola.drag)
+        .on("mouseup", function (d) {
+            d.fixed = 0;
+            d3Cola.alpha(1); // fire it off again to satify gridify
+        })
+        .on("click", function(d){
+            handleNodeClick(d.id);  
         });
 
-    link
-        .attr("d", function (d) {
-            var route = cola.makeEdgeBetween(
-                d.source.innerBounds,
-                d.target.innerBounds,
-                5
-            );
-            return lineFunction([route.sourceIntersection, route.arrowStart]);
-        })
-        .attr("x1", function (d) {
-            return d.source.x;
-        })
-        .attr("y1", function (d) {
-            return d.source.y;
-        })
-        .attr("x2", function (d) {
-            return d.target.x;
-        })
-        .attr("y2", function (d) {
-            return d.target.y;
-        });
+    //trying to remove nodes that have changed in node list for drawing...
+    console.log("Now removing nodes! \nnode.exit() --> ",node.exit())
+    console.log("\nnode.exit().remove() --> ", node.exit().remove())
+    //node.exit().remove()
 
-    node
-        .attr("x", function (d) {
-            return d.x - d.width / 2 + pad;
+    //inserting labels for nodes into svg
+    var label = svg
+        .selectAll(".label")
+        .data(nodes)
+        .enter()
+        .append("text")
+        .attr("class", "label")
+        .text(function (d) {
+            return d.name;
         })
-        .attr("y", function (d) {
-            return d.y - d.height / 2 + pad;
-        });
-    group
-        .attr("x", function (d) {
-            return d.bounds.x;
+        .call(d3Cola.drag);
+    
+    //appending title to node so when mouse hovers over node title is displayed
+    node.append("title").text(function (d) {
+        return d.name;
+    });
+
+    //inserting labels for groups into svg
+    var groupLabel = svg
+        .selectAll(".grouplabel")
+        .data(data.groups)
+        .enter()
+        .append("text")
+        .attr("class", "grouplabel")
+        .text(function (d) {
+            return d.name;
         })
-        .attr("y", function (d) {
-            return d.bounds.y;
-        })
-        .attr("width", function (d) {
-            return d.bounds.width();
-        })
-        .attr("height", function (d) {
-            return d.bounds.height();
-        });
-    label
-        .attr("x", function (d) {
+        .call(d3Cola.drag);
+
+    //appending title to group so when mouse hovers over node title is displayed
+    group.append("title").text(function (d) {
+        return d.name;
+    });
+
+    //creating lines
+    var lineFunction = d3
+        .line()
+        .x(function (d) {
             return d.x;
         })
-        .attr("y", function (d) {
-            var h = this.getBBox().height;
-            console.warn('label height', d.y)
-            return d.y + h / 4;
-        });
-    groupLabel
-        .attr("x", function (d) {
-            return d.bounds.x + d.bounds.width() / 2; // calculate x offset by dividing through group width
+        .y(function (d) {
+            return d.y;
         })
-        .attr("y", function (d) {
-            console.log("groupLabel", d.bounds.width());
-            return d.bounds.y + 18; // calculate y offset by adding the height of the groupLabel
-        });
-});
+        .curve(d3.curveLinear);
+
+    //layouting of webcola
+    d3Cola.on("tick", function () {
+        node
+            .each(function (d) {
+                d.innerBounds = d.bounds.inflate(-margin);
+            })
+            .attr("x", function (d) {
+                return d.innerBounds.x;
+            })
+            .attr("y", function (d) {
+                return d.innerBounds.y;
+            })
+            .attr("width", function (d) {
+                return d.innerBounds.width();
+            })
+            .attr("height", function (d) {
+                return d.innerBounds.height();
+            });
+
+        link
+            .attr("d", function (d) {
+                var route = cola.makeEdgeBetween(
+                    d.source.innerBounds,
+                    d.target.innerBounds,
+                    5
+                );
+                return lineFunction([route.sourceIntersection, route.arrowStart]);
+            })
+            .attr("x1", function (d) {
+                return d.source.x;
+            })
+            .attr("y1", function (d) {
+                return d.source.y;
+            })
+            .attr("x2", function (d) {
+                return d.target.x;
+            })
+            .attr("y2", function (d) {
+                return d.target.y;
+            });
+
+        node
+            .attr("x", function (d) {
+                return d.x - d.width / 2 + pad;
+            })
+            .attr("y", function (d) {
+                return d.y - d.height / 2 + pad;
+            });
+        group
+            .attr("x", function (d) {
+                return d.bounds.x;
+            })
+            .attr("y", function (d) {
+                return d.bounds.y;
+            })
+            .attr("width", function (d) {
+                return d.bounds.width();
+            })
+            .attr("height", function (d) {
+                return d.bounds.height();
+            });
+        label
+            .attr("x", function (d) {
+                return d.x;
+            })
+            .attr("y", function (d) {
+                var h = this.getBBox().height;
+                return d.y + h / 4;
+            });
+        groupLabel
+            .attr("x", function (d) {
+                return d.bounds.x + d.bounds.width() / 2; // calculate x offset by dividing through group width
+            })
+            .attr("y", function (d) {
+                return d.bounds.y + 18; // calculate y offset by adding the height of the groupLabel
+            });
+    });
+
+}
+
--- Visualization_of_Java_Code/src/main/java/visualization/D3/public/src/smallgrouped.js
+++ Visualization_of_Java_Code/src/main/java/visualization/D3/public/src/smallgrouped.js
@@ -1,93 +1,133 @@
 const graph = {
   "nodes": [
     {
+      "id": 1, 
+      "visibility": true,
       "name": "a",
       "width": 60,
       "height": 40
     },
     {
+      "id": 2, 
+      "visibility": true,
       "name": "b",
       "width": 60,
       "height": 40
     },
     {
+      "id": 3, 
+      "visibility": true,
       "name": "c",
       "width": 60,
       "height": 40
     },
     {
+      "id": 4, 
+      "visibility": true,
       "name": "d",
       "width": 60,
       "height": 40
     },
     {
+      "id": 5,  
+      "visibility": true,
       "name": "e",
       "width": 60,
       "height": 40
     },
     {
+      "id": 6,  
+      "visibility": true,
       "name": "f",
       "width": 60,
       "height": 40
     },
     {
+      "id": 7,  
+      "visibility": true,
       "name": "g",
       "width": 60,
       "height": 40
     },
     {
+      "id": 8, 
+      "visibility": true,
       "name": "b-to-c",
+      "width": 60,
+      "height": 10
+    }, 
+    {
+      "id": 404, 
+      "visibility": true,
+      "name": "MyVisibilityTest",
       "width": 60,
       "height": 10
     }
   ],
   "links": [
-    {
+    /*{
+      "id": 9,
       "name": "test",
       "source": 1,
-      "target": 7
+      "target": 7,
+      "visibility": true
     },
     {
+      "id": 10,
       "name": "test",
       "source": 7,
-      "target": 2
+      "target": 2,
+      "visibility": true
     },
     {
+      "id": 11,
       "name": "test",
       "source": 2,
-      "target": 3
+      "target": 3,
+      "visibility": true
     },
     {
+      "id": 12,
       "name": "test",
       "source": 3,
-      "target": 4
+      "target": 4,
+      "visibility": true
     },
     {
+      "id": 13,
       "name": "test",
       "source": 0,
-      "target": 1
+      "target": 1,
+      "visibility": true
     },
     {
+      "id": 14,
       "name": "test",
       "source": 2,
-      "target": 0
+      "target": 0,
+      "visibility": true
     },
     {
+      "id": 15,
       "name": "test",
       "source": 3,
-      "target": 5
+      "target": 5,
+      "visibility": true
     },
     {
+      "id": 16,
       "name": "test",
       "source": 0,
-      "target": 5
-    }
+      "target": 5,
+      "visibility": true
+    }*/
   ],
-  "groups": [
-    { "name": "myfirstgroup", "leaves": [0], "groups": [1] },
-    { "name": "mysecondgroup", "leaves": [1, 7, 2] },
-    { "name": "mythirdgroup", "leaves": [3, 4] }
+  "groups": [/*
+    { "id": 17, "visibility": true, "name": "myfirstgroup", "leaves": [0], "groups": [1] },
+    { "id": 18, "visibility": true, "name": "mysecondgroup", "leaves": [1, 7, 2] },
+    { "id": 19, "visibility": true, "name": "mythirdgroup", "leaves": [3, 4] }*/
   ]
 }
+
 
 export default graph
